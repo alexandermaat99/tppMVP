f33f006293d7765ac1eb18234e4770678402aa73
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2024-10-13 20:32:04 -0600

    cleanup

---
+++ thepatternsplace/app/protected/make-post/page.tsx
@@ -0,0 +1,22 @@
+import Hero from "@/components/hero";
+import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
+import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
+import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+
+export default function TestingAccess() {
+  return (
+    <div>
+      <h1>Testing Access</h1>
+
+      <form className="" action="">
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" id="email" />
+
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" />
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
